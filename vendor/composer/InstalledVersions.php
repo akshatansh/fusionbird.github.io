c3cc616764c991e560286bae0413395ee7934038
akshatansh <pintuisha2@gmail.com>
2022-12-19 01:54:12 +0530

    op

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -34,13 +34,7 @@ class InstalledVersions
         return array_keys(array_flip(\call_user_func_array('array_merge', $packages)));
     }
 
-    /**
-     * Returns a list of all package names with a specific type e.g. 'library'
-     *
-     * @param  string   $type
-     * @return string[]
-     * @psalm-return list<string>
-     */
+   
     public static function getInstalledPackagesByType($type)
     {
         $packagesByType = array();
@@ -56,15 +50,7 @@ class InstalledVersions
         return $packagesByType;
     }
 
-    /**
-     * Checks whether the given package is installed
-     *
-     * This also returns true if the package name is provided or replaced by another package
-     *
-     * @param  string $packageName
-     * @param  bool   $includeDevRequirements
-     * @return bool
-     */
+   
     public static function isInstalled($packageName, $includeDevRequirements = true)
     {
         foreach (self::getInstalled() as $installed) {
@@ -76,18 +62,7 @@ class InstalledVersions
         return false;
     }
 
-    /**
-     * Checks whether the given package satisfies a version constraint
-     *
-     * e.g. If you want to know whether version 2.3+ of package foo/bar is installed, you would call:
-     *
-     *   Composer\InstalledVersions::satisfies(new VersionParser, 'foo/bar', '^2.3')
-     *
-     * @param  VersionParser $parser      Install composer/semver to have access to this class and functionality
-     * @param  string        $packageName
-     * @param  string|null   $constraint  A version constraint to check for, if you pass one you have to make sure composer/semver is required by your package
-     * @return bool
-     */
+    
     public static function satisfies(VersionParser $parser, $packageName, $constraint)
     {
         $constraint = $parser->parseConstraints($constraint);
@@ -96,15 +71,7 @@ class InstalledVersions
         return $provided->matches($constraint);
     }
 
-    /**
-     * Returns a version constraint representing all the range(s) which are installed for a given package
-     *
-     * It is easier to use this via isInstalled() with the $constraint argument if you need to check
-     * whether a given version of a package is installed, and not just whether it exists
-     *
-     * @param  string $packageName
-     * @return string Version constraint usable with composer/semver
-     */
+   
     public static function getVersionRanges($packageName)
     {
         foreach (self::getInstalled() as $installed) {
@@ -132,10 +99,7 @@ class InstalledVersions
         throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
     }
 
-    /**
-     * @param  string      $packageName
-     * @return string|null If the package is being replaced or provided but is not really installed, null will be returned as version, use satisfies or getVersionRanges if you need to know if a given version is present
-     */
+    
     public static function getVersion($packageName)
     {
         foreach (self::getInstalled() as $installed) {
@@ -153,10 +117,7 @@ class InstalledVersions
         throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
     }
 
-    /**
-     * @param  string      $packageName
-     * @return string|null If the package is being replaced or provided but is not really installed, null will be returned as version, use satisfies or getVersionRanges if you need to know if a given version is present
-     */
+    
     public static function getPrettyVersion($packageName)
     {
         foreach (self::getInstalled() as $installed) {
@@ -174,10 +135,7 @@ class InstalledVersions
         throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
     }
 
-    /**
-     * @param  string      $packageName
-     * @return string|null If the package is being replaced or provided but is not really installed, null will be returned as reference
-     */
+    
     public static function getReference($packageName)
     {
         foreach (self::getInstalled() as $installed) {
@@ -195,10 +153,7 @@ class InstalledVersions
         throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
     }
 
-    /**
-     * @param  string      $packageName
-     * @return string|null If the package is being replaced or provided but is not really installed, null will be returned as install path. Packages of type metapackages also have a null install path.
-     */
+    
     public static function getInstallPath($packageName)
     {
         foreach (self::getInstalled() as $installed) {
@@ -212,10 +167,7 @@ class InstalledVersions
         throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
     }
 
-    /**
-     * @return array
-     * @psalm-return array{name: string, version: string, reference: string, pretty_version: string, aliases: string[], dev: bool, install_path: string}
-     */
+   
     public static function getRootPackage()
     {
         $installed = self::getInstalled();
@@ -223,20 +175,13 @@ class InstalledVersions
         return $installed[0]['root'];
     }
 
-    /**
-     * Returns the raw installed.php data for custom implementations
-     *
-     * @deprecated Use getAllRawData() instead which returns all datasets for all autoloaders present in the process. getRawData only returns the first dataset loaded, which may not be what you expect.
-     * @return array[]
-     * @psalm-return array{root: array{name: string, version: string, reference: string, pretty_version: string, aliases: string[], dev: bool, install_path: string}, versions: array<string, array{dev_requirement: bool, pretty_version?: string, version?: string, aliases?: string[], reference?: string, replaced?: string[], provided?: string[], install_path?: string}>}
-     */
+    
     public static function getRawData()
     {
         @trigger_error('getRawData only returns the first dataset loaded, which may not be what you expect. Use getAllRawData() instead which returns all datasets for all autoloaders present in the process.', E_USER_DEPRECATED);
 
         if (null === self::$installed) {
-            // only require the installed.php file if this file is loaded from its dumped location,
-            // and not from its source location in the composer/composer package, see https://github.com/composer/composer/issues/9937
+        
             if (substr(__DIR__, -8, 1) !== 'C') {
                 self::$installed = include __DIR__ . '/installed.php';
             } else {
@@ -247,45 +192,20 @@ class InstalledVersions
         return self::$installed;
     }
 
-    /**
-     * Returns the raw data of all installed.php which are currently loaded for custom implementations
-     *
-     * @return array[]
-     * @psalm-return list<array{root: array{name: string, version: string, reference: string, pretty_version: string, aliases: string[], dev: bool, install_path: string}, versions: array<string, array{dev_requirement: bool, pretty_version?: string, version?: string, aliases?: string[], reference?: string, replaced?: string[], provided?: string[], install_path?: string}>}>
-     */
+    /
     public static function getAllRawData()
     {
         return self::getInstalled();
     }
 
-    /**
-     * Lets you reload the static array from another file
-     *
-     * This is only useful for complex integrations in which a project needs to use
-     * this class but then also needs to execute another project's autoloader in process,
-     * and wants to ensure both projects have access to their version of installed.php.
-     *
-     * A typical case would be PHPUnit, where it would need to make sure it reads all
-     * the data it needs from this class, then call reload() with
-     * `require $CWD/vendor/composer/installed.php` (or similar) as input to make sure
-     * the project in which it runs can then also use this class safely, without
-     * interference between PHPUnit's dependencies and the project's dependencies.
-     *
-     * @param  array[] $data A vendor/composer/installed.php data set
-     * @return void
-     *
-     * @psalm-param array{root: array{name: string, version: string, reference: string, pretty_version: string, aliases: string[], dev: bool, install_path: string}, versions: array<string, array{dev_requirement: bool, pretty_version?: string, version?: string, aliases?: string[], reference?: string, replaced?: string[], provided?: string[], install_path?: string}>} $data
-     */
+    
     public static function reload($data)
     {
         self::$installed = $data;
         self::$installedByVendor = array();
     }
 
-    /**
-     * @return array[]
-     * @psalm-return list<array{root: array{name: string, version: string, reference: string, pretty_version: string, aliases: string[], dev: bool, install_path: string}, versions: array<string, array{dev_requirement: bool, pretty_version?: string, version?: string, aliases?: string[], reference?: string, replaced?: string[], provided?: string[], install_path?: string}>}>
-     */
+    
     private static function getInstalled()
     {
         if (null === self::$canGetVendors) {
@@ -308,8 +228,6 @@ class InstalledVersions
         }
 
         if (null === self::$installed) {
-            // only require the installed.php file if this file is loaded from its dumped location,
-            // and not from its source location in the composer/composer package, see https://github.com/composer/composer/issues/9937
             if (substr(__DIR__, -8, 1) !== 'C') {
                 self::$installed = require __DIR__ . '/installed.php';
             } else {
